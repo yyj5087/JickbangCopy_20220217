f114eebc4ffbfbb2bb182a7926be6d2e5504d61f
이 영 종 <naki5087@naver.com>
2022-02-17 16:59:12 +0900

    넘겨준 방 데이터 (Serializable 형태)를 jickData로 원복 - 실제 데이터 반영

--- app/src/main/java/com/nepplus/jickbangcopy_20220217/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/nepplus/jickbangcopy_20220217/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.nepplus.jickbangcopy_20220217
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nepplus.jickbangcopy_20220217.datas.JickbangData
 import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
@@ -10,9 +11,12 @@ class ViewRoomDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
 
-        val price = intent.getStringExtra("priceInfo")
-        txtPrice.text = price
+        val jickbangData = intent.getSerializableExtra("roomInfo") as JickbangData
 
+        txtprice.text = jickbangData.getFormattedPrice()
+        txtDescription.text = jickbangData.description
+        txtAddress.text = jickbangData.address
+        txtFloor.text = jickbangData.getFormattedFloor()
 
     }
 }
